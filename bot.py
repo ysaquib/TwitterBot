3cf8b080ca075d364b91daa9be7c537936b5a8d6
ysaquib <43067248+ysaquib@users.noreply.github.com>
2020-07-24 19:23:12 +0300

    Added Bot Script

---
+++ bot.py
@@ -0,0 +1,40 @@
+import time
+import random
+import tweepy
+
+auth = tweepy.OAuthHandler("[REDACTED]", "[REDACTED]")
+auth.set_access_token("[REDACTED]", "[REDACTED]")
+api = tweepy.API(auth)
+
+def tweet(tweeted):
+	allTweets = open("tweets.txt") 		# Open txt file containing all tweets
+	twt = random.randint(0, 182)
+	while (twt in tweeted):
+		twt = random.randint(0, 182) 	# Select random line
+	for i, line in enumerate(allTweets):
+		if (i == twt): 					# Find line and tweet it
+			print (line)
+			api.update_status(status=(line))
+			tweeted.append(i)			# Append line number to a list
+
+def main(lo, hi):
+	start = time.gmtime().tm_hour
+	tweeted = []
+	diff = 0
+	while (True):
+		current = time.gmtime().tm_hour
+
+		if (len(tweeted) > 60):			# Reset List
+			tweeted = []
+
+		if (current == start + diff):	# Check if its been diff hrs from start
+			tweet(tweeted)
+			start = current
+		
+		diff = random.randint(lo, hi)		
+		time.sleep(random.randint(30,5900))
+	return None
+
+
+main(1,1)
+
