59268d7d066efcc3c0c80cf0b31c5d6bc1263f57
ysaquib <43067248+ysaquib@users.noreply.github.com>
2020-07-27 18:52:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,23 +19,16 @@ def tweet(tweeted):
 			tweeted.append(i)	# Append line number to a list
 
 def main(lo, hi):
-	start = time.gmtime().tm_hour		# Start time on first execution
 	tweeted = []
-	diff = 0				# This is to tweet upon execution
 	while (True):
-		current = time.gmtime().tm_hour # Update current time in hours
-
-		if (len(tweeted) > 60):		# Reset List
+		if (len(tweeted) > 60):			# Reset List
 			tweeted = []
-
-		if (current == start + diff):	# Check if its been diff hrs from start
-			tweet(tweeted)
-			start = current		# Change start to the current time
 		
-		diff = random.randint(lo, hi)	# Get new diff based on random int
-		time.sleep(random.randint(30,1500))
+		tweet(tweeted)
+
+		time.sleep(random.randint(3600*lo,3600*hi))
 	return None
 
 
-main(2,5)					# Give range. Tweets range from every 2 to 5 hours
+main(2,6)					# Give range. Tweets range from every 2 to 5 hours
 
